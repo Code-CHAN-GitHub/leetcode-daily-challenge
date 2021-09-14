24755bfc53388ec0cb9cbd876346e7c3a9360f3a
chenjunjie <1648192188@qq.com>
2021-09-14 12:40:09 +0800

    2021-09-14

--- src/com/leetcode/daily/y2021/m09/d14_Q524.java
+++ src/com/leetcode/daily/y2021/m09/d14_Q524.java
@@ -5,29 +5,70 @@ import java.util.List;
 
 public class d14_Q524 {
 
+    /* 动态规划 */
     public String findLongestWord(String s, List<String> dictionary) {
         String res = "";
         char[] chars = s.toCharArray();
+        int n = chars.length;
+        int[][] dp = new int[n][128];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 'a'; j <= 'z'; j++) {
+                if (chars[i] == j)
+                    dp[i][j] = i;
+                else 
+                    dp[i][j] = i == n - 1 ? n : dp[i + 1][j];
+            }
+        }
         for (String str : dictionary) {
             char[] word = str.toCharArray();
             int i = 0, j = 0;
             while (i < chars.length && j < word.length) {
-                if (chars[i] == word[j])
-                    j++;
+                i = dp[i][word[j]];
+                if (i >= chars.length)
+                    break;
+                j++;
                 i++;
             }
-            if (j == word.length) {
-                if (str.length() > res.length() ||
+            if (j == word.length && 
                     (
-                        str.length() == res.length() &&
-                        str.compareTo(res) < 0
-                    )) {
-                    res = str;   
-                }
+                        str.length() > res.length() ||
+                        (
+                            str.length() == res.length() &&
+                            str.compareTo(res) < 0
+                        )
+                    )
+               ) {
+                res = str;
             }
         }
+        
         return res;
     }
+
+    /* 双指针 */
+    // public String findLongestWord(String s, List<String> dictionary) {
+    //     String res = "";
+    //     char[] chars = s.toCharArray();
+    //     for (String str : dictionary) {
+    //         char[] word = str.toCharArray();
+    //         int i = 0, j = 0;
+    //         while (i < chars.length && j < word.length) {
+    //             if (chars[i] == word[j])
+    //                 j++;
+    //             i++;
+    //         }
+    //         if (j == word.length) {
+    //             if (str.length() > res.length() ||
+    //                 (
+    //                     str.length() == res.length() &&
+    //                     str.compareTo(res) < 0
+    //                 )) {
+    //                 res = str;   
+    //             }
+    //         }
+    //     }
+    //     return res;
+    // }
 
     public static void main(String[] args) {
         String s = "abpcplea";
